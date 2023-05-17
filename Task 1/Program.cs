0a02e8ece6fa9ad4d2c6f44ea31c3ceed0e28a6b
Lebedev Vitaly <11132166@mail.ru>
2023-05-17 21:04:38 +0300

    Выполнено

---
+++ Task 1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int N = Prompt(" ");
+
+int Prompt(string message)
+{
+    Console.Write("Введите пятизначное число - ");
+    int N= int.Parse(Console.ReadLine()!);
+    return N;
+}
+
+Operation(N);
+
+void Operation(int number)
+{
+    if (number >10000 && number < 100000)
+    {
+        int A = (number/10000);
+        int B = (number/1000%10);
+        int C= (number/10%10);
+        int D = (number/1%10);
+        if (A==D)
+        {
+        if (B==C)
+        {
+        Console.WriteLine("Это палиндром");  
+        }
+        }
+        else
+        {
+           Console.WriteLine("Это не палиндром");  
+        }
+    }
+    else
+    {
+        Console.WriteLine("Данное число не пятизначно!");
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double x1 = Prompt("Введите координату X первой точки-");
+double y1 = Prompt("Введите координату y первой точки-");
+double z1 = Prompt("Введите координату z первой точки-");
+double x2 = Prompt("Введите координату X второй точки-");
+double y2 = Prompt("Введите координату y второй точки-");
+double z2 = Prompt("Введите координату z второй точки-");
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    double coordNumber = double.Parse(Console.ReadLine()!);
+    return coordNumber;
+}
+
+Match(x1, y1, x2, y2, z1, z2);
+
+void Match(double x1, double y1, double x2, double y2, double z1, double z2)
+{
+double A = (Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+(Math.Pow((z2-z1),2))));
+Console.WriteLine($"Расстояние между точкой А с координатамми {x1};{y1};{z1} и точкой B с координатами {x2};{y2};{z2} равно {A}");
+}
